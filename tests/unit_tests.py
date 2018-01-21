bd92fddfd86cf47fcba55a6344818b5a9d913ce1
Smeths <johnsmethurst@gmail.com>
2018-01-21 16:19:53 +0000

    add automated testing

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -5,9 +5,13 @@ class TestCalc(unittest.TestCase):
     
     def setup(self):
         self.G = Group("add",10)
+        self.G = Group("mult",9)
 
     def test_add(self):
         self.assertEqual(G.glist,[0,1,2,3,4,5,6,7,8,9])
+
+    def test_mult(self):
+        self.assertEqual(G.glist,[1,2,4,5,7,8])
 
 if __name__ == '__main__':
     unittest.main()
