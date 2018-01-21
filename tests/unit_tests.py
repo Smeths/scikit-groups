bb279e289d8e37a327b3c4701ef9a45b08e9f7ab
Smeths <johnsmethurst@gmail.com>
2018-01-21 16:23:58 +0000

    added comments

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -7,6 +7,8 @@ class TestCalc(unittest.TestCase):
         self.G = Group("add",10)
         self.G = Group("mult",9)
 
+# testing elements
+
     def test_add(self):
         self.assertEqual(G.glist,[0,1,2,3,4,5,6,7,8,9])
 
