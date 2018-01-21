2c33aaaf301d5b56d2dd27eadb362ea01dca06f2
Smeths <johnsmethurst@gmail.com>
2018-01-21 16:38:49 +0000

    updating test cases

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -5,15 +5,15 @@ class TestCalc(unittest.TestCase):
     
     def setup(self):
         self.G = Group("add",10)
-        self.G = Group("mult",9)
+        self.H = Group("mult",9)
 
 # testing elements
 
     def test_add(self):
-        self.assertEqual(G.glist,[0,1,2,3,4,5,6,7,8,9])
+        self.assertEqual(self.G.glist,[0,1,2,3,4,5,6,7,8,9])
 
     def test_mult(self):
-        self.assertEqual(G.glist,[1,2,4,5,7,8])
+        self.assertEqual(self.H.glist,[1,2,4,5,7,8])
 
 if __name__ == '__main__':
     unittest.main()
